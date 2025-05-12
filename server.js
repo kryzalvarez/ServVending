9ab097d7d85d520756bde7eda8359da63c63a38c
kryzalvarez <153542377+kryzalvarez@users.noreply.github.com>
2025-05-12 11:40:14 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { MercadoPagoConfig, Preference, Payment } = require("mercadopago");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const admin = require("firebase-admin");
-
+//ggg
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
